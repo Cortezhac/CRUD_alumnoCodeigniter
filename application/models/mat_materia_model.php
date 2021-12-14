1c968a3b61cfb303b56dc03ea85079b616f7cc9a
Irvin Cortez <irvincotez67@gmail.com>
2021-12-14 08:26:42 -0600

    Modelo materia

---
+++ application/models/mat_materia_model.php
@@ -0,0 +1,34 @@
+<?php if(!defined('BASEPATH')) exit('No direct script allowed');
+
+class mat_materia_model extends CI_Model {
+    function __construct()
+    {
+        $this->load->database();
+    }
+
+    function getMaterias(){
+        return $this->db->get('mat_materia')->result();
+    }
+
+    function setMateria($data){
+        return $this->db->insert('mat_materia', $data);
+    }
+
+    function getMateria($id){
+        return $this->db->get_where('mat_materia', array(
+            'mat_id' => $id
+        ));
+    }
+
+    function actualizar_materia($data, $id){
+        if($this->db->get_where('mat_materia', array('mat_id' => $id))->row() != null){
+            $this->db->where('mat_id', $id);
+            $resultado = $this->db->update('mat_materia', $data);
+            return $resultado;
+        }
+    }
+
+    function aliminar_materia($id){
+        return $this->db->delete('mat_materia', array('mat_id', $id));
+    }
+}
